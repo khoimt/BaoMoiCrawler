111d53afe9eac924342bbe47dab83be1368cca3e
khoimt <khoimt@live.com>
2014-06-02 08:00:37 +0700

    extract page content

--- src/newscrawler/BaoMoiCrawler.java
+++ src/newscrawler/BaoMoiCrawler.java
@@ -1,16 +1,25 @@
 package newscrawler;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Pattern;
 import newscrawler.crawler.Page;
 import newscrawler.crawler.WebCrawler;
 import newscrawler.parser.HtmlParseData;
 import newscrawler.url.WebURL;
-import java.util.List;
-import java.util.regex.Pattern;
 import org.apache.http.Header;
+import org.apache.http.entity.StringEntity;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Entities;
+import org.jsoup.select.Elements;
 
 public class BaoMoiCrawler extends WebCrawler {
 
-    private final static Pattern FILTERS = Pattern.compile(".*(\\.(css|js|bmp|gif|jpe?g"
+    protected final static Pattern FILTERS = Pattern.compile(".*(\\.(css|js|bmp|gif|jpe?g"
             + "|png|tiff?|mid|mp2|mp3|mp4"
             + "|wav|avi|mov|mpeg|ram|m4v|pdf"
             + "|rm|smil|wmv|swf|wma|zip|rar|gz))$");
@@ -24,41 +33,59 @@ public class BaoMoiCrawler extends WebCrawler {
 
     @Override
     public void visit(Page page) {
-        int docid = page.getWebURL().getDocid();
-        String url = page.getWebURL().getURL();
-        String domain = page.getWebURL().getDomain();
-        String path = page.getWebURL().getPath();
-        String subDomain = page.getWebURL().getSubDomain();
-        String parentUrl = page.getWebURL().getParentUrl();
-        String anchor = page.getWebURL().getAnchor();
-
-        System.out.println("Docid: " + docid);
-        System.out.println("URL: " + url);
-        System.out.println("Domain: '" + domain + "'");
-        System.out.println("Sub-domain: '" + subDomain + "'");
-        System.out.println("Path: '" + path + "'");
-        System.out.println("Parent page: " + parentUrl);
-        System.out.println("Anchor text: " + anchor);
-
         if (page.getParseData() instanceof HtmlParseData) {
-            HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
-            String text = htmlParseData.getText();
-            String html = htmlParseData.getHtml();
-            List<WebURL> links = htmlParseData.getOutgoingUrls();
-
-            System.out.println("Text length: " + text.length());
-            System.out.println("Html length: " + html.length());
-            System.out.println("Number of outgoing links: " + links.size());
-        }
-
-        Header[] responseHeaders = page.getFetchResponseHeaders();
-        if (responseHeaders != null) {
-            System.out.println("Response headers:");
-            for (Header header : responseHeaders) {
-                System.out.println("\t" + header.getName() + ": " + header.getValue());
+            String html = ((HtmlParseData) page.getParseData()).getHtml();
+            Document d;
+            try {
+                d = Jsoup.parse(new ByteArrayInputStream(html.getBytes("UTF-8")), "UTF-8", "http://www.baomoi.com");
+                d.outputSettings().escapeMode(Entities.EscapeMode.xhtml);
+                Elements e = d.select("div.article-body");
+                if (!e.isEmpty()) {
+                    insertPage(page, e);
+                }
+            } catch (IOException ex) {
+                Logger.getLogger(BaoMoiCrawler.class.getName()).log(Level.SEVERE, null, ex);
             }
+
         }
 
         System.out.println("=============");
+    }
+
+    public String getHTML(Elements e, String query, String defaultValue) {
+        Elements tmp = e.select(query);
+        if (!tmp.isEmpty()) {
+            String result = tmp.first().html().toString();
+//            result = result.replace("&lt;", "<")
+//                        .replace("&gt;", ">")
+//                        .replace("&amp;", "&")
+//                        .replace("&apos;", "'")
+//                        .replace("&quot;", "\"");
+            return result;
+        }
+        return defaultValue;
+    }
+
+    public String getText(Elements e, String query, String defaultValue) {
+        Elements tmp = e.select(query);
+        if (!tmp.isEmpty()) {
+            String result = tmp.first().text().toString();
+            return result;
+        }
+        return defaultValue;
+    }
+
+    public void insertPage(Page page, Elements article) {
+        String url = page.getWebURL().getURL();
+        String des = getText(article, "h2.summary", "");
+        String time = getText(article, "span.time", "");
+        String title = getText(article, "h1.title", "");
+        String content = getHTML(article, "div[itemprop=articleBody] table td", "");
+
+        System.out.println("URL: " + url);
+        System.out.println("des: " + des);
+        System.out.println("time: " + time);
+        System.out.println("title: " + title);
+        System.out.println("content: " + content);
     }
 }
--- src/newscrawler/fetcher/CustomFetchStatus.java
+++ src/newscrawler/fetcher/CustomFetchStatus.java
@@ -1,87 +1,60 @@
 package newscrawler.fetcher;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.apache.http.HttpStatus;
 
-/**
- * @author Yasser Ganjisaffar <lastname at gmail dot com>
- */
 public class CustomFetchStatus {
 
     public static final int PageTooBig = 1001;
     public static final int FatalTransportError = 1005;
     public static final int UnknownError = 1006;
 
+    protected static Map<Integer, String> statDesc;
+
+    static {
+        statDesc = new HashMap<>();
+        statDesc.put(HttpStatus.SC_OK, "OK");
+        statDesc.put(HttpStatus.SC_CREATED, "Created");
+        statDesc.put(new Integer(HttpStatus.SC_ACCEPTED), "Accepted");
+        statDesc.put(new Integer(HttpStatus.SC_NO_CONTENT), "No Content");
+        statDesc.put(new Integer(HttpStatus.SC_MOVED_PERMANENTLY), "Moved Permanently");
+        statDesc.put(new Integer(HttpStatus.SC_MOVED_TEMPORARILY), "Moved Temporarily");
+        statDesc.put(new Integer(HttpStatus.SC_NOT_MODIFIED), "Not Modified");
+        statDesc.put(new Integer(HttpStatus.SC_BAD_REQUEST), "Bad Request");
+        statDesc.put(new Integer(HttpStatus.SC_UNAUTHORIZED), "Unauthorized");
+        statDesc.put(new Integer(HttpStatus.SC_FORBIDDEN), "Forbidden");
+        statDesc.put(new Integer(HttpStatus.SC_INTERNAL_SERVER_ERROR), "Internal Server Error");
+        statDesc.put(new Integer(HttpStatus.SC_NOT_IMPLEMENTED), "Not Implemented");
+        statDesc.put(new Integer(HttpStatus.SC_BAD_GATEWAY), "Bad Gateway");
+        statDesc.put(new Integer(HttpStatus.SC_SERVICE_UNAVAILABLE), "Service Unavailable");
+        statDesc.put(new Integer(HttpStatus.SC_CONTINUE), "Continue");
+        statDesc.put(new Integer(HttpStatus.SC_TEMPORARY_REDIRECT), "Temporary Redirect");
+        statDesc.put(new Integer(HttpStatus.SC_METHOD_NOT_ALLOWED), "Method Not Allowed");
+        statDesc.put(new Integer(HttpStatus.SC_CONFLICT), "Conflict");
+        statDesc.put(new Integer(HttpStatus.SC_PRECONDITION_FAILED), "Precondition Failed");
+        statDesc.put(new Integer(HttpStatus.SC_REQUEST_TOO_LONG), "Request Too Long");
+        statDesc.put(new Integer(HttpStatus.SC_REQUEST_URI_TOO_LONG), "Request-URI Too Long");
+        statDesc.put(new Integer(HttpStatus.SC_MULTIPLE_CHOICES), "Multiple Choices");
+        statDesc.put(new Integer(HttpStatus.SC_SEE_OTHER), "See Other");
+        statDesc.put(new Integer(HttpStatus.SC_USE_PROXY), "Use Proxy");
+        statDesc.put(new Integer(HttpStatus.SC_PAYMENT_REQUIRED), "Payment Required");
+        statDesc.put(new Integer(HttpStatus.SC_NOT_ACCEPTABLE), "Not Acceptable");
+        statDesc.put(new Integer(HttpStatus.SC_PROXY_AUTHENTICATION_REQUIRED), "Proxy Authentication Required");
+        statDesc.put(new Integer(HttpStatus.SC_REQUEST_TIMEOUT), "Request Timeout");
+        statDesc.put(new Integer(PageTooBig), "Page size was too big");
+        statDesc.put(new Integer(FatalTransportError), "Fatal transport error");
+        statDesc.put(new Integer(UnknownError), "Unknown error");
+    }
+
+    ;
+
     public static String getStatusDescription(int code) {
-        switch (code) {
-            case HttpStatus.SC_OK:
-                return "OK";
-            case HttpStatus.SC_CREATED:
-                return "Created";
-            case HttpStatus.SC_ACCEPTED:
-                return "Accepted";
-            case HttpStatus.SC_NO_CONTENT:
-                return "No Content";
-            case HttpStatus.SC_MOVED_PERMANENTLY:
-                return "Moved Permanently";
-            case HttpStatus.SC_MOVED_TEMPORARILY:
-                return "Moved Temporarily";
-            case HttpStatus.SC_NOT_MODIFIED:
-                return "Not Modified";
-            case HttpStatus.SC_BAD_REQUEST:
-                return "Bad Request";
-            case HttpStatus.SC_UNAUTHORIZED:
-                return "Unauthorized";
-            case HttpStatus.SC_FORBIDDEN:
-                return "Forbidden";
-            case HttpStatus.SC_NOT_FOUND:
-                return "Not Found";
-            case HttpStatus.SC_INTERNAL_SERVER_ERROR:
-                return "Internal Server Error";
-            case HttpStatus.SC_NOT_IMPLEMENTED:
-                return "Not Implemented";
-            case HttpStatus.SC_BAD_GATEWAY:
-                return "Bad Gateway";
-            case HttpStatus.SC_SERVICE_UNAVAILABLE:
-                return "Service Unavailable";
-            case HttpStatus.SC_CONTINUE:
-                return "Continue";
-            case HttpStatus.SC_TEMPORARY_REDIRECT:
-                return "Temporary Redirect";
-            case HttpStatus.SC_METHOD_NOT_ALLOWED:
-                return "Method Not Allowed";
-            case HttpStatus.SC_CONFLICT:
-                return "Conflict";
-            case HttpStatus.SC_PRECONDITION_FAILED:
-                return "Precondition Failed";
-            case HttpStatus.SC_REQUEST_TOO_LONG:
-                return "Request Too Long";
-            case HttpStatus.SC_REQUEST_URI_TOO_LONG:
-                return "Request-URI Too Long";
-            case HttpStatus.SC_UNSUPPORTED_MEDIA_TYPE:
-                return "Unsupported Media Type";
-            case HttpStatus.SC_MULTIPLE_CHOICES:
-                return "Multiple Choices";
-            case HttpStatus.SC_SEE_OTHER:
-                return "See Other";
-            case HttpStatus.SC_USE_PROXY:
-                return "Use Proxy";
-            case HttpStatus.SC_PAYMENT_REQUIRED:
-                return "Payment Required";
-            case HttpStatus.SC_NOT_ACCEPTABLE:
-                return "Not Acceptable";
-            case HttpStatus.SC_PROXY_AUTHENTICATION_REQUIRED:
-                return "Proxy Authentication Required";
-            case HttpStatus.SC_REQUEST_TIMEOUT:
-                return "Request Timeout";
-            case PageTooBig:
-                return "Page size was too big";
-            case FatalTransportError:
-                return "Fatal transport error";
-            case UnknownError:
-                return "Unknown error";
-            default:
-                return "(" + code + ")";
+        Integer key = code;
+        if (statDesc.containsKey(key)) {
+            return (String) statDesc.get(key);
         }
+        return "(" + code + ")";
     }
 
 }
